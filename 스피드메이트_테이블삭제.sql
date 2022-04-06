208faf2bac0a55531905b55abe1b0cd3fcff2d4c
Lee JaeHyeong <dlwogud140@naver.com>
2022-04-06 15:52:38 +0900

    upload

---
+++ 스피드메이트_테이블삭제.sql
@@ -0,0 +1,14 @@
+DROP SEQUENCE repair_item_t_SEQ;
+drop table repair_item_t;
+
+DROP SEQUENCE receipt_t_SEQ;
+drop table receipt_t;
+
+DROP SEQUENCE customer_t_SEQ;
+drop table customer_t;
+
+DROP SEQUENCE staff_t_SEQ;
+drop table staff_t;
+
+DROP SEQUENCE car_t_SEQ;
+drop table car_t;
